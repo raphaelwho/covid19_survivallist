305cc0cfb0c1e6707b5c5cf3db863fc6ddf6607a
raphaelwho <71730759+raphaelwho@users.noreply.github.com>
2020-11-22 18:54:34 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+
+const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>
+          
+        </Text>
+        <Button title="Login" onPress={() => Alert.alert('Simple Button pressed')} />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+          
+        </Text>
+        <Button
+          title="Checklist"
+          color="#f194ff"
+          onPress={() => Alert.alert('Button with adjusted color pressed')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}></Text>
+        <Button title="Amazon" disabled onPress={() => Alert.alert('Cannot press this one')} />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+          
+        </Text>
+        <View style={styles.fixToText}>
+          <Button title="Left button" onPress={() => Alert.alert('Left button pressed')} />
+          <Button title="Right button" onPress={() => Alert.alert('Right button pressed')} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
